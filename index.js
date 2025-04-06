c880e50b789a5f80275f077a8540079fcdef0418
Caleb <clawrenceharris@gmail.com>
2025-04-05 21:09:16 -0400

    styles, html and file structure improved

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-window.onload = function () {
-  if (navigator.userAgent.indexOf("Chrome") !== -1) {
-    const h1 = document.querySelector("#home h1");
-
-    h1.style.fontFamily = "Saira Stencil One, sans-serif";
-  } else {
-    const h1 = document.querySelector("#home h1");
-    h1.style.fontFamily = "Stencil Cargo Army, sans-serif";
-  }
-};
-
 window.addEventListener("scroll", function () {
   let scrollPosition = window.scrollY;
   document.querySelector(".parallax-image").style.transform = `translateY(${
     scrollPosition * 0.2
   }px)`;
+});
+
+const h2 = document.querySelectorAll("h2");
+
+const rotationValues = [5, -3, 2, -4, -1, 1];
+window.onload(() => {
+  h2.forEach((element, i) => {
+    console.log(rotationValues[i]);
+    element.style.transform = `rotate(${rotationValues[i]})`;
+  });
 });
